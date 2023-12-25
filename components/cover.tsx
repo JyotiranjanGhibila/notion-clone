b9733443e7f41f388c9edb9d7219241db5372cbc
JyotiranjanGhibila <ghibilamohan@gmail.com>
2023-12-25 11:56:31 +0530

    fix: efficiently replace and remove the banner from db

--- components/cover.tsx
+++ components/cover.tsx
@@ -8,6 +8,7 @@ import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useParams } from "next/navigation";
 import { Id } from "@/convex/_generated/dataModel";
+import { useEdgeStore } from "@/lib/edgestore";
 
 interface CoverImageProps {
   url?: string;
@@ -18,8 +19,14 @@ export const Cover = ({ url, preview }: CoverImageProps) => {
   const params = useParams();
   const coverImage = useCoverImage();
   const removeCoverImage = useMutation(api.documents.removeCoverImage);
+  const { edgestore } = useEdgeStore();
 
-  const onRemove = () => {
+  const onRemove = async () => {
+    if (url) {
+      await edgestore.publicFiles.delete({
+        url: url,
+      });
+    }
     removeCoverImage({
       id: params.documentId as Id<"documents">,
     });
@@ -40,7 +47,7 @@ export const Cover = ({ url, preview }: CoverImageProps) => {
             right-5 flex items-center gap-x-2"
         >
           <Button
-            onClick={coverImage.onOpen}
+            onClick={() => coverImage.onReplace(url)}
             className="to-muted-foreground text-xs"
             variant="outline"
             size="sm"
